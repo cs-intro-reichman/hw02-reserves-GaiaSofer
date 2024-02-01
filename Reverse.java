727fd8976782f591088a7b3b43eea8be64d890c0
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-02-01 15:03:58 +0200

    updated 01/02/24 15:03

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,26 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
-	}
+		
+		String word = args[0];
+        int length = word.length() - 1;
+        int length1 = word.length() - 1;
+
+        // Print the reversed word
+        System.out.print("Reversed word: ");
+        for (int i = 0; i <= length1; i++) {
+            char ch = word.charAt(length);
+            System.out.print(ch);
+            length = length - 1;
+        }
+
+        // Print the middle character
+        if (length1 % 2 == 0) {
+            char ch2 = word.charAt(length1 / 2);
+            System.out.println("\nThe middle character is: " + ch2);
+        } else {
+            char ch3 = word.charAt(length1 / 2 + 1);
+            System.out.println("\nThe middle character is: " + ch3);
+        }
+    }
 }
