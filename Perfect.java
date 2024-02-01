7ad600d6df883f97afdd403f10cf587cf0d026ec
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-02-01 18:08:34 +0200

    updated 01/02/24 18:08

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,22 @@
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 /**
  *  Gets a command-line argument (int), and chekcs if the given number is perfect.
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		int perfectNumber = Integer.parseInt(args[0]);
+        String answer = "";
+
+        for (int i = 1; i < perfectNumber; i++) {
+            if (perfectNumber % i == 0) {
+                answer = answer + i;
+				if (i < perfectNumber / 2) {
+					answer = answer + " + ";
+				}
+            }
+        }
+
+        System.out.println(perfectNumber + " is a perfect number since " + perfectNumber + " = " + answer);
+    }
 }
