78386f6727761a7eab31c01ad57ca3b714c4a0b0
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-02-02 12:56:32 +0200

    02/02/24 12:56

--- Perfect.java
+++ Perfect.java
@@ -5,27 +5,19 @@ import javax.swing.text.StyledEditorKit.ForegroundAction;
  */
 public class Perfect {
 	public static void main (String[] args) {
-        if (args.length == 0) {
-            System.out.println("Please provide a number as a command-line argument.");
-            return;
-        }
-
         int perfectNumber = Integer.parseInt(args[0]);
         String answer = "";
-        boolean isPerfect = true;
-
         // Check if the number is a perfect number
         int sum = 0;
         for (int i = 1; i < perfectNumber; i++) {
             if (perfectNumber % i == 0) {
-                sum += i;
+                sum ++;
                 answer = answer + i;
                 if (i < perfectNumber / 2) {
                     answer = answer + " + ";
                 }
             }
         }
-
         // Check if the sum of divisors equals the original number
         if (sum * 2 == perfectNumber) {
             System.out.println(perfectNumber + " is a perfect number since " + perfectNumber + " = " + answer);
