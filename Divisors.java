c73da185728cdc20f3ee2d30366d01b3adc7be30
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-02-02 11:38:55 +0200

    01/02 11:36

--- Divisors.java
+++ Divisors.java
@@ -3,11 +3,8 @@
  */
 public class Divisors {
     public static void main(String[] args) {
-        // Extract the input number from the command line arguments
-        int number = Integer.parseInt(args[0]);
-        
-        // Print divisors of the given number
-        System.out.println("Divisors of " + number + ":");
+    int number = Integer.parseInt(args[0]);
+    System.out.println("Divisors of " + number + ":");
         
         // Iterate from 1 to the given number
         for (int i = 1; i <= number; i++) {
