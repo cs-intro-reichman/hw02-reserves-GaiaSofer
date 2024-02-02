4a1c8c4e089e32f3fdeec775bb132260946cc255
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-02-02 20:23:16 +0200

    02/02/24 20:23

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -11,69 +11,71 @@ import java.util.Random;
 
 public class OneOfEachStats {
     public static void main(String[] args) {
-		int T = Integer.parseInt(args[0]);
-		
-		// Initializes counters
-		int numberOf2Children = 0;
-		int numberOf3Children = 0;
-		int numberOf4OrMoreChildren = 0;
-		
-		// The total number of children born in all T simulations.
-		double sumNumberOfChildren = 0;
-		
-		// Executes the simulation, T times
-		for (int i = 0; i < T; i++) {
-			boolean boy = false;
-		    boolean girl = false;
-		    int count = 0;
-		    while (!(boy && girl)) {
-			    double rnd = Math.random();
-			    if (rnd < 0.5) {
-				    boy = true;
-				    ////System.out.print("b ");
-			    } else {
-				    girl = true;
-				    ////System.out.print("g ");
-			    }
-			    count++;
-		    }
-		    ////System.out.println();
-		    ////System.out.println(count + " children.");
-			
-			// Update the counters
-			sumNumberOfChildren = sumNumberOfChildren + count; 
-			
-			if (count == 2) {
-				numberOf2Children++;
-			} else {
-			    if (count == 3) {
-			    	numberOf3Children++;
-			    } else {
-			        numberOf4OrMoreChildren++;
-			    }
+				int T = Integer.parseInt(args[0]);
+				int seed = Integer.parseInt(args[1]);
+				Random generator = new Random(seed);
+				
+				// Initializes counters
+				int numberOf2Children = 0;
+				int numberOf3Children = 0;
+				int numberOf4OrMoreChildren = 0;
+				
+				// The total number of children born in all T simulations.
+				double sumNumberOfChildren = 0;
+				
+				// Executes the simulation, T times
+				for (int i = 0; i < T; i++) {
+					boolean boy = false;
+					boolean girl = false;
+					int count = 0;
+					while (!(boy && girl)) {
+						double rnd = generator.nextDouble();
+						if (rnd < 0.5) {
+							boy = true;
+							////System.out.print("b ");
+						} else {
+							girl = true;
+							////System.out.print("g ");
+						}
+						count++;
+					}
+					//System.out.println();
+					//System.out.println(count + " children.");
+					
+					// Update the counters
+					sumNumberOfChildren = sumNumberOfChildren + count; 
+					
+					if (count == 2) {
+						numberOf2Children++;
+					} else {
+						if (count == 3) {
+							numberOf3Children++;
+						} else {
+							numberOf4OrMoreChildren++;
+						}
+					}
+				}
+				
+				double avreage = sumNumberOfChildren / T;
+				
+				System.out.println("Average: " + avreage + " children to get at least one of each gender.");
+				System.out.println("Number of families with 2 children: " + numberOf2Children);
+				System.out.println("Number of families with 3 children: " + numberOf3Children);
+				System.out.println("Number of families with 4 or more children: " + numberOf4OrMoreChildren);
+				
+				// Calculates the maximum of all counters, and finds to which case it belongs
+				String mode = "The most common number of children is ";
+				int max = Math.max(Math.max(numberOf2Children, numberOf3Children),
+								   numberOf4OrMoreChildren);
+				if (max == numberOf2Children) {
+					mode = mode + "2.";
+				} else {
+					if (max == numberOf3Children) {
+						mode = mode + "3.";
+					} else {
+						mode = mode + "4 or more.";
+					}
+				}
+				System.out.println(mode);
 			}
-		}
-		
-		double avreage = sumNumberOfChildren / T;
-		
-		System.out.println("Average: " + avreage + " children to get at least one of each gender.");
-		System.out.println("Number of families with 2 children: " + numberOf2Children);
-		System.out.println("Number of families with 3 children: " + numberOf3Children);
-		System.out.println("Number of families with 4 or more children: " + numberOf4OrMoreChildren);
-		
-		// Calculates the maximum of all counters, and finds to which case it belongs
-		String mode = "The most common number of children is ";
-		int max = Math.max(Math.max(numberOf2Children, numberOf3Children),
-			               numberOf4OrMoreChildren);
-		if (max == numberOf2Children) {
-			mode = mode + "2.";
-		} else {
-			if (max == numberOf3Children) {
-				mode = mode + "3.";
-			} else {
-			    mode = mode + "4 or more.";
-			}
-		}
-		System.out.println(mode);
-	}
-}
+		}	
