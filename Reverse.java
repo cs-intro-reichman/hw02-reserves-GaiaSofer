bd16e4687e7baf79399c9a038cc23f2ffdc976cf
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-02-02 08:45:56 +0200

    02/02/24 08:44

--- Reverse.java
+++ Reverse.java
@@ -3,21 +3,17 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-		
-		String word = args[0];
+    public static void main(String[] args) {
+        // Extract the input word from the command line arguments
+        String word = args[0];
         int length = word.length() - 1;
         int length1 = word.length() - 1;
-
-        // Print the reversed word
         System.out.print("Reversed word: ");
         for (int i = 0; i <= length1; i++) {
             char ch = word.charAt(length);
             System.out.print(ch);
             length = length - 1;
         }
-
-        // Print the middle character
         if (length1 % 2 == 0) {
             char ch2 = word.charAt(length1 / 2);
             System.out.println("\nThe middle character is: " + ch2);
